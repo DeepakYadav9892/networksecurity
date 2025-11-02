b8706379b8e292b21d820c2a46a2960889ac71ff
DeepakYadav9892 <deepak989241@gmail.com>
2025-11-02 10:30:32 +0530

    Project final commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 '''
-The setup.py file is an essential part of packaging and distributing 
-python porjects.It is used by setuptools (or distutils in older python versionn)
-to define the configuration of your project , such as its meta data 
-, dependencies and more 
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
 '''
 
 from setuptools import find_packages,setup
@@ -10,30 +10,30 @@ from typing import List
 
 def get_requirements()->List[str]:
     """
-    This function will return list of requirements 
+    Thiss function will return list of requirements
+    
     """
     requirement_lst:List[str]=[]
     try:
         with open('requirements.txt','r') as file:
-            ##Read line form the file 
+            #Read lines from the file
             lines=file.readlines()
-            ##Process each line 
+            ## Process each line
             for line in lines:
                 requirement=line.strip()
-                ##ingnore the empty line and -e.
-                if requirement and requirement!='-e .':
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
                     requirement_lst.append(requirement)
     except FileNotFoundError:
-        print("Requirements.txt file not found")
+        print("requirements.txt file not found")
 
     return requirement_lst
+
 setup(
-    name="Networksecurity",
+    name="NetworkSecurity",
     version="0.0.1",
-    author="DeepakYadav",
+    author="Krish Naik",
+    author_email="krishnaik06@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
-  )
-
-                     
-
+)
--- test_mongodb.py
+++ test_mongodb.py
@@ -1,8 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://deepakitjob9892_db_user:<password>2utbecx.mongodb.net/?appName=Cluster1"
+uri = "mongodb+srv://krishaitechnologies:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
